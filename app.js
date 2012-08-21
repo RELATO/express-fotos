7e802e1dc2b1255ec57f5412239c41104d06a58d
Bojan Djurkovic <dbojan@gmail.com>
2012-08-21 01:10:43 -0300

    added user middleware, user persistance, menu

--- app.js
+++ app.js
@@ -5,11 +5,15 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , login = require('./routes/login')
+  , register = require('./routes/register')
   , photos = require('./routes/photos')
   , http = require('http')
   , path = require('path');
 
 var app = express();
+
+var user = require('./lib/middleware/user');
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -17,10 +21,13 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
+  app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
+  //app.use(express.session());
+  app.use(express.cookieSession());
+  app.use(user);
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -31,6 +38,11 @@ app.configure('development', function(){
 
 //app.get('/', routes.index);
 app.get('/', photos.list);
+app.get('/login', login.form);
+app.post('/login', login.submit);
+app.get('/logout', login.logout);
+app.get('/register', register.form);
+app.post('/register', register.submit);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
